69c48fea818074d85d6c12d77bd22fd5dc382b7d
You <thibaultyou@gmail.com>
2019-05-03 03:57:54 +0200

    feat: Adding GraphQL API server

---
+++ schema.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLUnionType
+} = require("graphql");
+
+const BookType = new GraphQLObjectType({
+  name: "BookType",
+  fields: () => ({
+    isbn: { type: GraphQLString },
+    title: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    cover: { type: GraphQLString },
+    synopsis: { type: new GraphQLList(GraphQLString) }
+  })
+});
+
+const OfferType = new GraphQLObjectType({
+  name: "Offer",
+  fields: () => ({
+    type: { type: GraphQLString },
+    sliceValue: { type: GraphQLInt },
+    value: { type: GraphQLInt }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return axios
+          .get("http://henri-potier.xebia.fr/books")
+          .then(res => res.data);
+      }
+    },
+    offers: {
+      type: new GraphQLList(OfferType),
+      args: {
+        isbns: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(
+            `http://henri-potier.xebia.fr/books/${args.isbns}/commercialOffers`
+          )
+          .then(res => res.data.offers);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
