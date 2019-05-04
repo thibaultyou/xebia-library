026aec0ceeae030bb6bdb6413c1293852848312b
You <thibaultyou@gmail.com>
2019-05-04 16:19:15 +0200

    feat: Adding chalk for logging

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const chalk = require('chalk');
+const log = console.log;
 const schema = require('./schema');
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const isDevEnv = NODE_ENV == 'development';
+log(chalk.cyan(`Starting server on ${NODE_ENV} environment`));
+
+const app = express();
 const fallback = isDevEnv
   ? `<a href='http://localhost:${PORT}/graphql'>http://localhost:${PORT}/graphql</a> for testing !`
   : 'Hello from Express.js !';
-
-const app = express();
 
 app.get('/', function(req, res) {
   res.send(fallback);
@@ -23,4 +26,4 @@ app.use(
   }),
 );
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => log(chalk.green(`Server started on port ${PORT}`)));
