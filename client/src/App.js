94ceb0b67e3312bb1ed730d5f2370adfaf171bb3
You <thibaultyou@gmail.com>
2019-05-04 20:57:16 +0200

    feat: Adding routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home component ...</h1>;
+const Basket = () => <h1>Basket component ...</h1>;
+const Fallback = () => <h1>Fallback component ...</h1>;
 
 function App() {
-  return <h1>Please insert rainbows here ...</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/basket" exact component={Basket} />
+        <Route component={Fallback} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
